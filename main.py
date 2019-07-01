ca9699441ebf4ae3fb17d9710444773379d6b3d0
Holly Parker <hollyfparker@gmail.com>
2019-07-01 18:40:25 -0500

    another commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
+
 form = """
 <!DOCTYPE html>
 <html>
@@ -21,11 +23,15 @@ form = """
     <body>
       <form method="post">
         <label>
-         Rotate by:   
-            <input type="text" name="Username"/>
-            <input type="password" name="Password"/>
-            <input type="password" name="Verify Password"/>
-            <input type="text" name="Email (optional)"/>
+         Signup:<br> 
+            Username:  
+            <input type="text" name="username" required/><br>
+            Password:
+            <input type="password" name="password" required/><br>
+            Verify Password:
+            <input type="password" name="verify" required/><br>
+            Email (optional)  
+            <input type="text" name="email (optional)"/><br>
         
             
         </label>
@@ -34,3 +40,20 @@ form = """
     </body>
 </html>
 """
+@app.route("/")
+def index():
+    return form.format("")
+
+@app.route("/", methods=['POST'])
+def psw_verify():
+    password = request.form['password']
+    orig_password = request.form['password']
+    verify_password = request.form['verify']
+    username = request.form['username']
+    for password in password:
+        if orig_password == verify_password:
+            return ("Hello, " + username)
+        else:
+            return "Passwords must match!" 
+  
+app.run()
