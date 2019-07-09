928b642025309de10da7f3ba6459f0ee5f1b95ba
Holly Parker <hollyfparker@gmail.com>
2019-07-08 19:46:57 -0500

    another

--- main.py
+++ main.py
@@ -1,48 +1,13 @@
-from flask import Flask, request 
+from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
 
-        </style>
-    </head>
-    <body>
-      <form method="post">
-        <label>
-         Signup:<br> 
-            Username:  
-            <input type="text" name="username" minlength="3" maxlength="20" required/><br>
-            Password:
-            <input type="password" name="password" minlength="3" maxlength="20" required/><br>
-            Verify Password:
-            <input type="password" name="verify" minlength="3" maxlength="20" required/><br>
-            Email (optional)  
-            <input type="text" name="email (optional)" minlength="3" maxlength="20"/><br>
-        
-            
-        </label>
-        <input type="submit"/>
-    </form>
-    </body>
-</html>
-"""
 @app.route("/")
 def index():
-    return form.format("")
-
-@app.route("/", methods=['POST'])
+    return render_template('index.html')
+    
+    @app.route("/", methods=['POST'])
 def psw_verify():
     password = request.form['password']
     orig_password = request.form['password']
